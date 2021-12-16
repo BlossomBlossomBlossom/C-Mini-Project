41862e4400ff030f908e080de41b981f77e30750
arthurespartinez25 <arthur.espartinez25@gmail.com>
2021-12-16 20:11:31 +0800

    Update mini.c

--- mini.c
+++ mini.c
@@ -1,7 +1,5 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<string.h>
-
 typedef struct product{
 	int id;
 	char description[20];
@@ -14,40 +12,50 @@ void create(){
 	FILE *fp;
 	product *p; 
 	int n, i;
-	printf("Enter how many products you want to add : ");
+	printf("Enter how many students you want : ");
 	scanf("%d", &n);
-	fflush(stdin);
 	
 	p = (product*)calloc(n, sizeof(product));
-	fp = fopen("Products.csv", "w");
+	fp = fopen("Inventory.txt", "w");
 	
 	for(i=0; i<n; i++){
 		printf("Enter ID No: ");
 		scanf("%d,", &p[i].id);
 		fflush(stdin);
 		printf("Enter Product Description: ");
-		scanf("%[^\n]s,", &p[i].description);
+		scanf("%[^\n]s,", p[i].description);
 		printf("Enter Quantity: ");
 		scanf("%f,", &p[i].quantity);
 		fflush(stdin);
-		printf("Enter Expiration Date: (dd/mm/yyyy)");
-		scanf("%[^\n]s,", &p[i].expiration);
+		printf("Enter Expiration Date (dd/mm/yyyy): ");
+		scanf("%[^\n]s,", p[i].expiration);
 		fflush(stdin);
-		printf("Enter Price: (Php)");
+		printf("Enter Price (Php): ");
 		scanf("%f,\n", &p[i].price);
 		fflush(stdin);
 		
-		fprintf(fp,"\n%d,%s,%f,%s,%f",p[i].id,p[i].description,p[i].quantity,p[i].expiration,p[i].price);
+		fwrite(&p[i],sizeof(product),1,fp);
+	}
+	fclose(fp);
+}
+void display(){
+	product p1;
+	FILE *fp;
+	fp = fopen("Inventory.txt", "r");
+	
+	while(fread(&p1, sizeof(product),1,fp)){
+		printf("\n%-20d%-20s%-20.2f%-20s%-20.2f",p1.id,p1.description,p1.quantity,p1.expiration,p1.price);
 	}
 	fclose(fp);
 }
 void update(){
 	product p1;
+	char ch;
 	FILE *fp, *fp1;
 	int id, found = 0;
-	fp = fopen("Products.csv", "r");
-	fp1 = fopen("temp.csv", "w");
-	printf("Enter product ID to Update : ");
+	fp = fopen("Inventory.txt", "r");
+	fp1 = fopen("temp.txt", "w");
+	printf("Enter ID no. to Update : ");
 	scanf("%d", &id);
 	
 	while(fread(&p1, sizeof(product),1,fp)){
@@ -55,32 +63,83 @@ void update(){
 		if(p1.id == id){
 			found=1;
 			fflush(stdin);
-			printf("Enter new ID: ");
-			scanf("%d", p1.id);
-			printf("Enter new Decsription: ");
-			scanf("%[^\n]s", p1.description);
-			printf("Enter new Decsription: ");
-			scanf("%lf", p1.quantity);
-			printf("Enter new Expiration Date (dd/mm/yyyy): \n");
-			scanf("%[^\n]s", p1.expiration);
-			printf("Enter new Price (Php): \n");
-			scanf("%f", p1.price);
+			printf("Would you like to update product ID [Y/N]?");
+			scanf("%c", &ch);
+			if(ch == 'y' || ch == 'Y'){
+				fflush(stdin);
+				printf("Enter New ID: ");
+				scanf("%d", &p1.id);
+			} else if (ch == 'n' || ch == 'N'){
+			} else {
+				printf("Invalid Selection!");
+				main();
+			}
+			fflush(stdin);
+			printf("Would you like to update product Description [Y/N]?");
+			scanf("%c", &ch);
+			fflush(stdin);
+			if(ch == 'y' || ch == 'Y'){
+				fflush(stdin);
+				printf("Enter New Product Description: ");
+				scanf("%[^\n]s,", p1.description);
+			} else if (ch == 'n' || ch == 'N'){
+			} else {
+				printf("Invalid Selection!");
+				main();
+			}
+			fflush(stdin);
+			printf("Would you like to update product quantity [Y/N]?");
+			scanf("%c", &ch);
+			if(ch == 'y' || ch == 'Y'){
+				printf("Enter New Quantity: ");
+				scanf("%f,", &p1.quantity);
+			} else if (ch == 'n' || ch == 'N'){
+			} else {
+				printf("Invalid Selection!");
+				main();
+			}
+			fflush(stdin);
+			printf("Would you like to update product date [Y/N]?");
+			scanf("%c", &ch);
+			if(ch == 'y' || ch == 'Y'){
+				printf("Enter New Expiration Date (dd/mm/yyyy): ");
+				fflush(stdin);
+				scanf("%[^\n]s,", p1.expiration);
+			} else if (ch == 'n' || ch == 'N'){
+			} else {
+				printf("Invalid Selection!");
+				main();
+			}
+			fflush(stdin);
+			printf("Would you like to update product price [Y/N]?");
+			scanf("%c", &ch);
+			if(ch == 'y' || ch == 'Y'){
+				fflush(stdin);
+				printf("Enter New Price (Php): ");
+				scanf("%f,\n", &p1.price);
+			} else if (ch == 'n' || ch == 'N'){
+			} else {
+				printf("Invalid Selection!");
+				main();
+			}
 		}
 		fwrite(&p1,sizeof(product),1,fp1);
+		
 	}
 	fclose(fp);
 	fclose(fp1);
 	if(found){
-		fp1 = fopen("Products.csv", "r");
-		fp = fopen("temp.csv", "w");
+		fp1 = fopen("temp.txt", "r");
+		fp = fopen("Inventory.txt", "w");
 		
 		while(fread(&p1,sizeof(product),1,fp1)){
 			fwrite(&p1,sizeof(product),1,fp);
 		}
 		fclose(fp);
 		fclose(fp1);
+		printf("Product succesfully updated!");
 	} else {
-		printf("\nProduct ID does not exist\n");	
+		printf("\nRecord Not Found\n");
 	}
 }
 
@@ -88,7 +147,8 @@ int main(){
 	int ch;
 	do{
 		printf("\n1.CREATE");
-		printf("\n2.UPDATE");
+		printf("\n2.DISPLAY");
+		printf("\n3.UPDATE");
 		
 		printf("\nEnter Your Choice : ");
 		scanf("%d", &ch);
@@ -98,9 +158,11 @@ int main(){
 				create();
 				break;
 			case 2:
+				display();
+				break;
+			case 3:
 				update();
 				break;
-
 		}
 	} while(ch!=0);
 	
