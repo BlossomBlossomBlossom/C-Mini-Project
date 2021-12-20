e9410595f185accdc45a1bb2cd97119ca13a1ecc
arthurespartinez25 <arthur.espartinez25@gmail.com>
2021-12-20 08:08:12 +0800

    Update update function

--- InventorySystem.c
+++ InventorySystem.c
@@ -339,6 +339,265 @@ void view()
 
     fclose(fpin);
 }
+int checkDate(char date[])
+{
+	int digit = 0;
+	int hyphen = 0;
+	int i=0;
+	int j = 0;
+	int k;
+	int l;
+	int year =0;
+	int month=0;
+	int day=0;
+	for (i = 0; date[i] != '\0'; i++)
+    {
+        if (isdigit(date[i]) != 0)
+            digit++;
+        else if (ispunct(date[i]) != 0 && date[i] == '-')
+            hyphen++;
+    }
+    if((digit == 8 || digit == 7 || digit == 6) && hyphen == 2){
+    	for(j = 0; j < 4; j++)
+		{
+			if (isdigit(date[j]) != 0)
+            	year++;
+		}
+		for(k=5; k <= 6; k++)
+		{
+			if (isdigit(date[k]) != 0)
+			month++;
+		}
+		if(year == 4)
+		{
+			char yearString[4] = {date[0], date[1], date[2], date[3]};
+			int num1 = atoi(yearString);
+			int yearChecker = checkYear(num1);
+			if(month == 1)
+			{
+				char monthString[1] = {date[5]};
+				int num2 = atoi(monthString);
+				int monthChecker = checkMonth(num2);
+				for(l=7; date[l] != '\0'; l++)
+				{
+					if (isdigit(date[l]) != 0)
+	            	day++;
+				}
+				if(day == 1)
+				{
+					char dayString[1] = {date[7]};
+					int num3 = atoi(dayString);
+					int dayChecker = checkDay(num3, num2, num1);
+					return dayChecker + monthChecker + yearChecker;
+				} else
+				{
+					char dayString[2] = {date[7], date[8]};
+					int num3 = atoi(dayString);
+					int dayChecker = checkDay(num3, num2, num1);
+					return dayChecker + monthChecker + yearChecker;
+				}
+				
+			} else if(month == 2)
+			{
+				char monthString[2] = {date[5], date[6]};
+				int num2 = atoi(monthString);
+				int monthChecker = checkMonth(num2);
+				for(l=8; date[l] != '\0'; l++)
+				{
+					if (isdigit(date[l]) != 0)
+	            	day++;
+				}
+				if(day == 1)
+				{
+					char dayString[1] = {date[8]};
+					int num3 = atoi(dayString);
+					int dayChecker = checkDay(num3, num2, num1);
+					return dayChecker + monthChecker + yearChecker;
+				} else
+				{
+					char dayString[2] = {date[8], date[9]};
+					int num3 = atoi(dayString);
+					int dayChecker = checkDay(num3, num2, num1);
+					return dayChecker + monthChecker + yearChecker;
+				}
+			} else {
+				return 1;
+			}
+		} else {
+			return 1;
+		}
+	}  else {
+		return 1;
+	}
+}
+
+int checkYear(int year)
+{
+	if(year < 2022 || year > 2500)
+	{
+		return year;
+	} else
+	{
+		return 0;
+	}
+}
+int checkMonth(int month)
+{
+	if(month <1 || month > 12)
+	{
+		return month;
+	} else {
+		return 0;
+	}
+}
+int checkDay(int day, int month, int year)
+{
+	if(day >31 && (month ==1 || month ==3 || month ==5 || month ==7 || month ==8 || month ==10 || month ==12))
+	{
+		return day;
+	} else if(day > 30 && (month == 4 || month == 6 || month == 9 || month == 11))
+	{
+		return day;
+	} else if (day > 29 && month == 2 && year %4 == 0)
+	{
+		return day;
+	} else if(day > 28 && month == 2 && year %4 != 0)
+	{
+		return day;
+	} else	
+	{
+		return 0;
+	}
+}
+int check(char input[])
+{
+	int alpha = 0;
+	int punct = 0;
+	int period = 0;
+	int i=0;
+	int total;
+	for (i = 0; input[i] != '\0'; i++)
+    {
+        if (isalpha(input[i]) != 0)
+            alpha++;
+        else if (ispunct(input[i]) != 0 && input[i] != '.')
+            punct++;
+        else if (input[i] == '.')
+        	period++;
+    }
+    total = alpha + punct;
+    if(total > 0){
+    	return total;
+	} else if(period > 1){
+		return period;
+	} else {
+		return 0;
+	}
+}
+int checkIdInput(char input[])
+{
+	int alpha = 0;
+	int punct = 0;
+	int digit = 0;
+	int i=0;
+	int total;
+	for (i = 0; input[i] != '\0'; i++)
+    {
+        if (isalpha(input[i]) != 0)
+            alpha++;
+        else if (ispunct(input[i]) != 0)
+            punct++;
+        else if (isdigit(input[i]) != 0)
+        	digit++;
+    }
+    total = alpha + punct;
+    if(total > 0){
+    	return total;
+	} else if(digit != 5){
+		return digit;
+	} else {
+		return 0;
+	}
+}
+int checkId (int n)
+{
+    FILE *fpin;
+    product p[511];
+    char row[100];
+    char *token;
+    char *temp[5];
+    char temp2[7];
+    char temp3[5];
+    int i = 0;
+    int j = 0;
+    int x = 0;
+    int flag = 0;
+
+    fpin    = fopen("Inventory_ST_NoBOM.csv", "r");
+
+    //read input
+    while(feof(fpin) == 0)
+    {
+        fgets(row, 100, fpin);
+        token = strtok(row, ",");
+
+        while(token != NULL)
+        {
+          temp[i++] = token;
+          token = strtok(NULL, ",");
+
+        }
+        if(strlen(temp[0]) == 7)
+        {
+            strncpy(temp2, temp[0], 7);
+            x=0;
+            while(x !=5)
+            {
+                temp3[x] = temp2[x+1];
+                x++;
+            }
+            p[j].id = atoi(temp3);
+        }
+        else
+        {
+            p[j].id = atoi(temp[0]);
+        }
+
+        strcpy(p[j].desc,   temp[1]);
+        strncpy(p[j].desc, p[j].desc+1, strlen(p[j].desc));
+        p[j].desc[strlen(p[j].desc)-1]    = '\0';
+
+        strcpy(p[j].qty,    temp[2]);
+        strncpy(p[j].qty, p[j].qty+1, strlen(p[j].qty));
+        p[j].qty[strlen(p[j].qty)-1]    = '\0';
+
+        strcpy(p[j].exp,    temp[3]);
+        strncpy(p[j].exp, p[j].exp+1, strlen(p[j].exp));
+        p[j].exp[strlen(p[j].exp)-1]    = '\0';
+
+        strcpy(p[j].price,  temp[4]);
+        strncpy(p[j].price, p[j].price+1, strlen(p[j].price));
+        if(p[j].price[strlen(p[j].price) - 1] == '\n')
+        {
+            p[j].price[strlen(p[j].price)-1]  = '\0';
+            p[j].price[strlen(p[j].price)-2]  = '\0';
+        }
+        else
+            p[j].price[strlen(p[j].price)-1]  = '\0';
+
+        j++;
+        i = 0;
+    }
+    for(x = 0; x < j ; x++)
+    {
+        if(p[x].id == n)
+		{
+			flag = 1;
+		}
+    }
+fclose(fpin);
+return flag;
+}
 void update()
 {
     FILE *fpin;
@@ -355,10 +614,12 @@ void update()
     int x = 0;
     int id = 0;
     int newid;
+    int found = 0;
     char newdesc[53];
     char newqty[18];
     char newexp[18];
     char newprice[18];
+    char tempId[20];
 
     fpin    = fopen("Inventory_ST_NoBOM.csv", "r");
 
@@ -424,76 +685,110 @@ void update()
         if(p[x].id == id)
 		{
 
-			printf("Would you like to update product ID [Y/N]?");
+			found=1;
+			printf("\nWould you like to update product ID [Y/N]? ");
 			scanf(" %c", &ch);
+			while (ch != 'y' && ch != 'Y' && ch !='n' && ch != 'N'){
+				printf("Invalid Selection. Please try again. ");
+				scanf(" %c", &ch);
+			}
 			if(ch == 'y' || ch == 'Y'){
 
 				printf("Enter New ID: ");
 				scanf("%d", &p[x].id);
-			} else if (ch == 'n' || ch == 'N'){
-			} else {
-				printf("Invalid Selection!");
+				sprintf(tempId, "%d", p[x].id);
+				while(checkIdInput(tempId) !=0)
+				{
+					printf("Invalid. Must be 5 digit integer. ");
+					scanf("%d", &p[x].id);
+					sprintf(tempId, "%d", p[x].id);
+				}
+				while(checkId(p[x].id) != 0)
+				{
+					printf("ID already taken. Please try again. ");
+					scanf("%d", &p[x].id);
+				}
 			}
-
-			printf("Would you like to update product Description [Y/N]?");
+			printf("\nWould you like to update product Description [Y/N]? ");
 			scanf(" %c", &ch);
-
+			while (ch != 'y' && ch != 'Y' && ch !='n' && ch != 'N'){
+				printf("Invalid Selection. Please try again. ");
+				scanf(" %c", &ch);
+			}
 			if(ch == 'y' || ch == 'Y'){
 
 				printf("Enter New Product Description: ");
-				scanf("%s", &p[x].desc);
-			} else if (ch == 'n' || ch == 'N'){
-			} else {
-				printf("Invalid Selection!");
+				scanf("%[^\n]s", &p[x].desc);
 			}
-
-			printf("Would you like to update product quantity [Y/N]?");
+			
+			printf("\nWould you like to update product quantity [Y/N]? ");
 			scanf(" %c", &ch);
+			while (ch != 'y' && ch != 'Y' && ch !='n' && ch != 'N'){
+				printf("Invalid Selection. Please try again. ");
+				scanf(" %c", &ch);
+			}
 			if(ch == 'y' || ch == 'Y'){
 				printf("Enter New Quantity: ");
 				scanf("%s,", &p[x].qty);
-			} else if (ch == 'n' || ch == 'N'){
-			} else {
-				printf("Invalid Selection!");
+				while(check(p[x].qty) != 0)
+				{
+					printf("Invalid input. Must be greater than or equal to 0. ");
+					scanf("%s,", &p[x].qty);
+				}
 			}
-
-			printf("Would you like to update product date [Y/N]?");
+			printf("\nWould you like to update product expiration date [Y/N]?");
 			scanf(" %c", &ch);
+			while (ch != 'y' && ch != 'Y' && ch !='n' && ch != 'N'){
+				printf("Invalid Selection. Please try again. ");
+				scanf(" %c", &ch);
+			}
 			if(ch == 'y' || ch == 'Y'){
-				printf("Enter New Expiration Date (yyyy-mm-dd): ");
-
+				printf("Enter New Expiration Date (yyyy-mm-dd). \n");
 				scanf("%s", &p[x].exp);
-			} else if (ch == 'n' || ch == 'N'){
-			} else {
-				printf("Invalid Selection!");
+				while(checkDate(p[x].exp) != 0)
+				{
+					printf("Invalid input. Please try again (yyyy-mm-dd). ");
+					scanf("%s", &p[x].exp);
+				}
 			}
 
-			printf("Would you like to update product price [Y/N]?");
+			printf("\nWould you like to update product price [Y/N]?");
 			scanf(" %c", &ch);
+			while (ch != 'y' && ch != 'Y' && ch !='n' && ch != 'N'){
+				printf("Invalid Selection. Please try again. ");
+				scanf(" %c", &ch);
+			}
 			if(ch == 'y' || ch == 'Y'){
 
 				printf("Enter New Price (Php): ");
-				scanf("%s,\n", &p[x].price);
-			} else if (ch == 'n' || ch == 'N'){
-			} else {
-				printf("Invalid Selection!");
+				scanf("%s", &p[x].price);
+				while(check(p[x].price) != 0 || strcmp(&p[x].price, "0") == 0)
+				{
+					printf("Invalid input. Must be greater than 0. ");
+					scanf("%s,\n", &p[x].price);
+				}
 			}
 		}
     }
 
     fclose(fpin);
-    fpout   = fopen("Inventory_ST_NoBOM.csv", "w");
-    for(x = 0; x < j ; x++)
-    {
-        fprintf(fpout, "\"%d\",\"%s\",\"%s\",\"%s\",\"%s\"\n",p[x].id, p[x].desc, p[x].qty, p[x].exp, p[x].price );
-    }
-
-    printf("Updated!");
-    getch();
-    fclose(fpout);
-
-
-
+    if(!found)
+	{
+		printf("Product not on the list!\n");
+		getch();
+		main();
+	} else
+	{
+		fpout   = fopen("Inventory_ST_NoBOM.csv", "w");
+	    for(x = 0; x < j ; x++)
+	    {
+	        fprintf(fpout, "\"%d\",\"%s\",\"%s\",\"%s\",\"%s\"\n",p[x].id, p[x].desc, p[x].qty, p[x].exp, p[x].price );
+	    }
+	
+	    printf("Updated!");
+	    getch();
+	    fclose(fpout);
+	}
 }
 int main()
 {
